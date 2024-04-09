38117fe84c4f8becd6ee12319833b46228891355
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2024-04-09 21:21:18 +0200

    refactor

--- minpack_numba/__init__.py
+++ minpack_numba/__init__.py
@@ -1,13 +1,55 @@
-"""_summary_
-"""
-# TODO cminpack wrapper
-# TODO wrap other functions
+"""A numba wrapper for the minpack library."""
 
-from .minpack_numba import lmdif1, sig_minpack_func2
+__version__ = "0.1.0"
+
+from .chkder import chkder
+from .dpmpar import dpmpar
+from .hybrd import hybrd, hybrd1, hybrd1_, hybrd_
+from .hybrj import hybrj, hybrj1, hybrj1_, hybrj_
+from .lmder import lmder, lmder1, lmder1_, lmder_
+from .lmdif import lmdif, lmdif1, lmdif1_, lmdif_
+from .lmstr import lmstr, lmstr1, lmstr1_, lmstr_
 from .signatures import (
-    sig_minpack_func,
-    sig_minpack_func2,
-    sig_minpack_fcn_hybrj,
-    sig_minpack_fcn_lmder,
-    sig_minpack_fcn_lmstr,
+    MinpackSignature,
+    hybrd_sig,
+    hybrj_sig,
+    lmder_sig,
+    lmdif_sig,
+    lmstr_sig,
 )
+from .utils import address_as_void_pointer
+
+__all__ = [
+    # Utils
+    "address_as_void_pointer",
+    # Signature
+    "hybrd_sig",
+    "hybrj_sig",
+    "lmdif_sig",
+    "lmder_sig",
+    "lmstr_sig",
+    "MinpackSignature",
+    # Functions
+    "chkder",
+    "dpmpar",
+    "hybrd",
+    "hybrd_",
+    "hybrd1",
+    "hybrd1_",
+    "hybrj",
+    "hybrj_",
+    "hybrj1",
+    "hybrj1_",
+    "lmder",
+    "lmder_",
+    "lmder1",
+    "lmder1_",
+    "lmdif",
+    "lmdif_",
+    "lmdif1",
+    "lmdif1_",
+    "lmstr1",
+    "lmstr1_",
+    "lmstr",
+    "lmstr_",
+]
--- minpack_numba/signatures.py
+++ minpack_numba/signatures.py
@@ -1,54 +1,184 @@
-"""_summary_
+"""Signatures for the functions passed to the minpack functions."""
+
+from __future__ import annotations
+
+__all__ = [
+    "hybrd_sig",
+    "hybrj_sig",
+    "lmdif_sig",
+    "lmder_sig",
+    "lmstr_sig",
+    "MinpackSignature",
+]
+
+from typing import TYPE_CHECKING
+
+from numba import types
+
+if TYPE_CHECKING:
+    from numba.core.typing import Signature
+
+_ptr_double = types.CPointer(types.float64)
+_ptr_int = types.CPointer(types.int32)
+
+
+class MinpackSignature:
+    """Signatures for the functions passed to the minpack functions."""
+
+    @staticmethod
+    def hybrd(udata_type: types.Type = types.voidptr) -> Signature:
+        """Signature for the function passed to the `hybrd` function.
+
+        Parameters
+        ----------
+        udata_type : types.Type, optional
+            The type of the user data, by default types.voidptr
+
+        Returns
+        -------
+        Signature
+            The signature of the function
+
+        """
+        return types.void(
+            types.int32,  # n
+            _ptr_double,  # *x
+            _ptr_double,  # *fvec
+            _ptr_int,  # *iflag
+            udata_type,  # udata
+        )
+
+    @staticmethod
+    def hybrj(udata_type: types.Type = types.voidptr) -> Signature:
+        """Signature for the function passed to the `hybrj` function.
+
+        Parameters
+        ----------
+        udata_type : types.Type, optional
+            The type of the user data, by default types.voidptr
+
+        Returns
+        -------
+        Signature
+            The signature of the function
+
+        """
+        return types.void(
+            types.int32,  # n
+            _ptr_double,  # *x
+            _ptr_double,  # *fvec
+            _ptr_double,  # *fjac
+            types.int32,  # ldfjac
+            _ptr_int,  # *iflag
+            udata_type,  # udata
+        )
+
+    @staticmethod
+    def lmdif(udata_type: types.Type = types.voidptr) -> Signature:
+        """Signature for the function passed to the `lmdif` function.
+
+        Parameters
+        ----------
+        udata_type : types.Type, optional
+            The type of the user data, by default types.voidptr
+
+        Returns
+        -------
+        Signature
+            The signature of the function
+
+        """
+        return types.void(
+            types.int32,  # m
+            types.int32,  # n
+            _ptr_double,  # *x
+            _ptr_double,  # *fvec
+            _ptr_int,  # *iflag
+            udata_type,  # udata
+        )
+
+    @staticmethod
+    def lmder(udata_type: types.Type = types.voidptr) -> Signature:
+        """Signature for the function passed to the `lmder` function.
+
+        Parameters
+        ----------
+        udata_type : types.Type, optional
+            The type of the user data, by default types.voidptr
+
+        Returns
+        -------
+        Signature
+            The signature of the function
+
+        """
+        return types.void(
+            types.int32,  # m
+            types.int32,  # n
+            _ptr_double,  # *x
+            _ptr_double,  # *fvec
+            _ptr_double,  # *fjac
+            types.int32,  # ldfjac
+            _ptr_int,  # *iflag
+            udata_type,  # udata
+        )
+
+    @staticmethod
+    def lmstr(udata_type: types.Type = types.voidptr) -> Signature:
+        """Signature for the function passed to the `lmstr` function.
+
+        Parameters
+        ----------
+        udata_type : types.Type, optional
+            The type of the user data, by default types.voidptr
+
+        Returns
+        -------
+        Signature
+            The signature of the function
+
+        """
+        return types.void(
+            types.int32,  # m
+            types.int32,  # n
+            _ptr_double,  # *x
+            _ptr_double,  # *fvec
+            _ptr_double,  # *fjrow
+            _ptr_int,  # *iflag
+            udata_type,  # udata
+        )
+
+
+# func
+hybrd_sig = MinpackSignature.hybrd()
+"""
+(n: int32, x: float64*, fvec: float64*, iflag: int32*, udata: void*) -> none
 """
 
-import numba as nb
+# fcn_hybrj
+hybrj_sig = MinpackSignature.hybrj()
+"""
+(n: int32, x: float64*, fvec: float64*, fjac: float64*, ldfjac: int32, iflag: int32*,
+    udata: void*) -> none
+"""
 
-sig_minpack_func = nb.types.void(
-    nb.types.int32,  # n
-    nb.types.CPointer(nb.types.double),  # x
-    nb.types.CPointer(nb.types.double),  # fvec
-    nb.types.CPointer(nb.types.int32),  # iflag
-    nb.types.CPointer(nb.types.double),  # udata
-)
+# func2
+lmdif_sig = MinpackSignature.lmdif()
+"""
+(m: int32, n: int32, x: float64*, fvec: float64*, iflag: int32*, udata: void*)
+    -> none
+"""
 
-sig_minpack_func2 = nb.types.void(
-    nb.types.int32,  # m
-    nb.types.int32,  # n
-    nb.types.CPointer(nb.types.double),  # x
-    nb.types.CPointer(nb.types.double),  # fvec
-    nb.types.CPointer(nb.types.int32),  # iflag
-    nb.types.CPointer(nb.types.double),  # udata
-)
+# fcn_lmder
+lmder_sig = MinpackSignature.lmder()
+"""
+(m: int32, n: int32, x: float64*, fvec: float64*, fjac: float64*, ldfjac: int32,
+    iflag: int32*, udata: void*) -> none
+"""
 
-# TODO check this
-sig_minpack_fcn_hybrj = nb.types.void(
-    nb.types.int32,  # n
-    nb.types.CPointer(nb.types.double),  # x
-    nb.types.int32,  # ldfjac
-    nb.types.CPointer(nb.types.double),  # fvec
-    nb.types.CPointer(nb.types.double),  # fjac
-    nb.types.CPointer(nb.types.int32),  # iflag
-    nb.types.CPointer(nb.types.double),  # udata
-)
-
-sig_minpack_fcn_lmder = nb.types.void(
-    nb.types.int32,  # m
-    nb.types.int32,  # n
-    nb.types.CPointer(nb.types.double),  # x
-    nb.types.CPointer(nb.types.double),  # fvec
-    nb.types.CPointer(nb.types.double),  # fjac
-    nb.types.int32,  # ldfjac
-    nb.types.CPointer(nb.types.int32),  # iflag
-    nb.types.CPointer(nb.types.double),  # udata
-)
-
-# TODO check this
-sig_minpack_fcn_lmstr = nb.types.void(
-    nb.types.int32,  # m
-    nb.types.int32,  # n
-    nb.types.CPointer(nb.types.double),  # x
-    nb.types.CPointer(nb.types.double),  # fvec
-    nb.types.CPointer(nb.types.double),  # fjrow
-    nb.types.CPointer(nb.types.int32),  # iflag
-    nb.types.CPointer(nb.types.double),  # udata
-)
+# fcn_lmstr
+lmstr_sig = MinpackSignature.lmstr()
+"""
+(m: int32, n: int32, x: float64*, fvec: float64*, fjrow: float64*, iflag: int32*,
+    udata: void*) -> none
+"""
